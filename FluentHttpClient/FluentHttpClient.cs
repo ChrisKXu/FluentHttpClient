5c39aba89bb28b57c8e80cbe2c1fd2389fb40a6d
Christopher Xu <chriskxu@hotmail.com>
2015-12-29 22:31:15 +0800

    initial project

---
+++ FluentHttpClient/FluentHttpClient.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FluentHttpClient
+{
+    public class FluentHttpClient
+    {
+        public FluentHttpClient()
+        {
+        }
+    }
+}
