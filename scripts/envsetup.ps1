c70df5bab1ecc2497447355b5e3563fcde09296a
Christopher Xu <chriskxu@hotmail.com>
2015-12-31 00:27:24 +0800

    let install clr instead of coreclr in envsetup.ps1

--- scripts/envsetup.ps1
+++ scripts/envsetup.ps1
@@ -2,7 +2,7 @@
 
 $Branch='dev'
 iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.ps1'))
-dnvm install latest -r coreclr -arch x86 -alias default
+dnvm install latest -r clr -arch x86 -alias default
 
 # Restore dependencies
 dnu restore
