fa663dd755245eb68daf0e8704d77e662ca95124
Christopher Xu <ChrisKXu@hotmail.com>
2016-01-12 14:13:50 +0800

    Added FluentHttpRequest and FluentHttpResponse classes

--- FluentHttpClient/FluentHttpClient.cs
+++ FluentHttpClient/FluentHttpClient.cs
@@ -1,14 +1,37 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace FluentHttpClient
 {
-    public class FluentHttpClient
+    public class FluentHttpClient : IDisposable
     {
+        internal HttpMessageInvoker HttpClient { get; set; }
+
+        /// <summary>
+        /// Default contructor, takes no arguments
+        /// </summary>
         public FluentHttpClient()
+            : this(new HttpClient())
         {
+        }
+
+        internal FluentHttpClient(HttpMessageInvoker httpClient)
+        {
+            // internal constructor for test purposes
+            HttpClient = httpClient;
+        }
+
+        public FluentHttpRequest CreateHttpRequest()
+        {
+            return new FluentHttpRequest(this);
+        }
+        
+        public void Dispose()
+        {
+            HttpClient?.Dispose();
         }
     }
 }
---
+++ FluentHttpClient/FluentHttpRequest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FluentHttpClient
+{
+    public class FluentHttpRequest
+    {
+        internal FluentHttpClient HttpClient { get; set; }
+
+        internal FluentHttpRequest(FluentHttpClient httpClient)
+        {
+            HttpClient = httpClient;
+        }
+
+        public virtual Task<FluentHttpResponse> SendAsync()
+        {
+            return SendAsync(CancellationToken.None);
+        }
+
+        public virtual Task<FluentHttpResponse> SendAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
